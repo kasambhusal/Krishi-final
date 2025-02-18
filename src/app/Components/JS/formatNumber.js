c5cf48249860690588db041ab45cc4e1f4e36993
kasam <kasamsaving@gmail.com>
2025-02-18 20:13:40 +0545

    done

--- src/app/Components/JS/formatNumber.js
+++ src/app/Components/JS/formatNumber.js
@@ -6,13 +6,12 @@
  * @returns {string} The formatted number
  */
 export const formatNumber = (num) => {
-    if (num >= 1e9) {
-      return (num / 1e9).toFixed(1) + 'B'; // Billion
-    } else if (num >= 1e6) {
-      return (num / 1e6).toFixed(1) + 'M'; // Million
-    } else if (num >= 1e3) {
-      return (num / 1e3).toFixed(1) + 'K'; // Thousand
-    }
-    return num.toString();
-  };
-  
+  if (num >= 1e9) {
+    return (num / 1e9).toFixed(1) + "B"; // Billion
+  } else if (num >= 1e6) {
+    return (num / 1e6).toFixed(1) + "M"; // Million
+  } else if (num >= 1e3) {
+    return (num / 1e3).toFixed(1) + "K"; // Thousand
+  }
+  return num.toString();
+};
